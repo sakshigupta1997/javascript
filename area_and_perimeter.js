97af42a00dea8efcc791c77a801fdf5245fdb383
sakshigupta1997 <40806036+sakshigupta1997@users.noreply.github.com>
2018-08-31 23:46:19 +0530

    area_and_perimeter

---
+++ area_and_perimeter.js
@@ -0,0 +1,13 @@
+let length=3
+let width=4
+
+const area_of_reactangle;
+let parameter_of_reactangle;
+
+area_of_reactangle=length*width;
+
+parameter_of_reactangle=2*(length+width);
+
+console.log(area_of_reactangle);
+
+console.log(parameter_of_reactangle);
