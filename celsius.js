3ee30c9c35f3abd92fbb1710e815fedf2eae6490
Sakshi Gupta <40806036+sakshigupta1997@users.noreply.github.com>
2018-09-05 23:50:27 +0530

    update

--- celsius.js
+++ celsius.js
@@ -1,23 +1,20 @@
-// We are writing a program to convert celcius to Farenhite.
 
-// Initialize celsius variable here
-// npm install readline-sync
 let readline = require('readline-sync'); // this readline sync is a good module to start with.
 
 let celsius;
 
 celsius = readline.question("put temp in Celsius:");
 
-// write the equation to convert celsius to farenhite
+
 
 // Tf = Tc * (9/5) + 32
 
-// convert it to program to pass it to your computer.
+
 
 let farenhite;
 
 farenhite = celsius * (9/5) + 32;
 
-// print result to console.
+
 
 console.log(celsius + " C is equal to "+ farenhite + " F");
